7702929308f4aece37f0cd30a459d6d3b1470fe4
anyahall <athall67@gmail.com>
2016-07-06 11:31:58 -0400

    Add files via upload
    with edited convolution (matlab conv function)

--- field_shift_new.m
+++ field_shift_new.m
@@ -1,11 +1,11 @@
-
+%% integral
 
 
 R = 0.015; % m
 N = 128;
 [x,y]=meshgrid(linspace(-10*R,10*R,N));
 B0 = 3; % [Tesla]
-%chi_b=0.273*4*pi; %deox blood
+chi_b=0.273*4*pi; %deox blood
 chi_w = -8e-6; % susceptibility of water
 r = sqrt(x.^2+y.^2);
 r = r(:);
@@ -19,9 +19,9 @@ yv=y(:);
 chi_dist=[];
 for i=1:length(r)
     if abs(r(i))<=R
-        chi_dist(i)=chi_w;
+        chi_dist(i)=chi_b;
     else
-        chi_dist(i)=0;
+        chi_dist(i)=chi_w;
     end
 end
 
@@ -67,8 +67,61 @@ gamma=42.57e6;
 F1 = gamma*B;
 F2 = gamma*Ba;
 F2 = reshape(F2,[N N]);
+figure 
 imshow([F1 F2 F2-F1],[]); colormap jet
+title('Field shift [Hz] sphere of blood immersed in water (Left to Right: convolution, analytical, difference)');
 
 % %figure
 % imshow(dF,[]); colormap jet; colorbar
 % title('B-field shift [Hz]')
+
+
+
+%% convolution
+
+
+R = 0.015; % m
+N = 128;
+[x,y]=meshgrid(linspace(-10*R,10*R,N));
+B0 = 3; % [Tesla]
+chi_b=0.273*4*pi; %deox blood
+chi_w = -8e-6; % susceptibility of water
+r = sqrt(x.^2+y.^2);
+r = r(:);
+dx = 10*R/N;
+dy = 10*R/N;
+
+xv=x(:);
+yv=y(:);
+
+
+
+chi_dist=[];
+for i=1:length(r)
+    if abs(r(i))<=R
+        chi_dist(i)=chi_b;
+    else
+        chi_dist(i)=chi_w;
+    end
+end
+
+dB=[]; 
+for i=1:length(r)
+        a=[xv(i) yv(i)];
+        b=[0 1]; %B0 vector
+        theta=findAngle(a,b);
+        d = (1/(4*pi))*((3*(cos(theta)^2)-1)/abs(r(i))^3);
+        dB(i) = conv(chi_dist(i),d);
+        dB(i) = dB(i)*B0;
+    
+end
+
+
+dB=reshape(dB,[128 128]); % local field shift
+figure
+imshow(dB,[]); colormap jet; colorbar;
+axis on
+title('sphere field shift (convolution)');
+ % convolving after loop with full-size 'd' and 'chi' matrices 
+                 % results in a dB matrix of the wrong dimensions.
+                 % Therefore, convolution must be element-wise in this case
